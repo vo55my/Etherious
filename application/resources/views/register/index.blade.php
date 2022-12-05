9acb954db1388f6c32b12d89309190bf33202344
Andrie Firmansyah <andriefirmansyah334@gmail.com>
2022-12-05 21:46:06 +0700

    register

--- application/resources/views/register/index.blade.php
+++ application/resources/views/register/index.blade.php
@@ -10,25 +10,46 @@
 </head>
 <body class="text-center">
   <main class="form-signin w-100 m-auto">
-    <form method="post">
+    <form action="/register" method="post">
+      @csrf 
       <h2 class="mb-3">Shinakamana</h2>
       <h3 class="h3 mb-3 fw-normal text-center">Registration Form</h3>
   
       <div class="form-floating">
-        <input type="text" name="name" class="form-control border-0" id="name" placeholder="Name">
+        <input type="text" name="name" class="form-control border-0 @error('name')is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}>
         <label for="name">Name</label>
+        @error('name')
+          <div class="invalid-feedback">
+            {{ message }}
+          </div>
+        @enderror
       </div>
       <div class="form-floating">
-        <input type="text" name="username" class="form-control border-0" id="username" placeholder="Username">
+        <input type="text" name="username" class="form-control border-0 @error('username')is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}>
         <label for="username">Username</label>
+        @error('name')
+          <div class="invalid-feedback">
+            {{ message }}
+          </div>
+        @enderror
       </div>
       <div class="form-floating">
-        <input type="email" name="email" class="form-control border-0" id="email" placeholder="name@example.com">
+        <input type="email" name="email" class="form-control border-0 @error('email')is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}>
         <label for="email">Email address</label>
+        @error('name')
+          <div class="invalid-feedback">
+            {{ message }}
+          </div>
+        @enderror
       </div>
       <div class="form-floating">
-        <input type="password" name="password" class="form-control border-0" id="password" placeholder="Password">
+        <input type="password" name="password" class="form-control border-0 @error('password')is-invalid @enderror" id="password" placeholder="Password" required>
         <label for="password">Password</label>
+        @error('name')
+          <div class="invalid-feedback">
+            {{ message }}
+          </div>
+        @enderror
       </div>
   
       <div class="checkbox mb-3">
--- application/routes/web.php
+++ application/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'register']);
