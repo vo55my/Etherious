f33f4c9f69018373a79f6841b9cbcd44f20df06f
fresaaaaa <fresabayu0601@gmail.com>
2022-12-08 17:44:26 +0700

    menambah database

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,21 @@
 
 namespace Database\Seeders;
 
+// use App\Models\Commune;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
+// use Tests\TestCase;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+// use App\Models\Post;
+
+
 
 class DatabaseSeeder extends Seeder
 {
+    use RefreshDatabase, InteractsWithDatabase;
+
     /**
      * Seed the application's database.
      *
@@ -13,8 +24,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
 
-        // ])
+        //
+
+        User::factory(5)->create();
+
+        // User::create([
+        //     'image' => 'fresa.jpg',
+        //     'name' => 'Fresabayu',
+        //     'username' => 'Fresaa',
+        //     'email' => 'fresabayu123@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
     }
 }
--- application/resources/views/register/index.blade.php
+++ application/resources/views/register/index.blade.php
@@ -13,11 +13,11 @@
   <main class="form-signin w-100 m-auto">
     <form action="/register" method="post">
       @csrf
-      <h2 class="mb-3">Shinakamana</h2>
+      <h2 class="mb-3 display-4">Shinakamana</h2>
       <h3 class="h3 mb-3 fw-normal text-center">Registration Form</h3>
 
       <div class="form-floating">
-        <input type="text" name="name" class="form-control border-0 @error('name')is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}>
+        <input type="text" name="name" class="form-control border-0 @error('name')is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
         <label for="name">Name</label>
         @error('name')
           <div class="invalid-feedback">
@@ -26,7 +26,7 @@
         @enderror
       </div>
       <div class="form-floating">
-        <input type="text" name="username" class="form-control border-0 @error('username')is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}>
+        <input type="text" name="username" class="form-control border-0 @error('username')is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}">
         <label for="username">Username</label>
         @error('username')
           <div class="invalid-feedback">
@@ -35,7 +35,7 @@
         @enderror
       </div>
       <div class="form-floating">
-        <input type="email" name="email" class="form-control border-0 @error('email')is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}>
+        <input type="email" name="email" class="form-control border-0 @error('email')is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
         <label for="email">Email address</label>
         @error('email')
           <div class="invalid-feedback">
@@ -46,7 +46,7 @@
       <div class="form-floating">
         <input type="password" name="password" class="form-control border-0 @error('password')is-invalid @enderror" id="password" placeholder="Password" required>
         <label for="password">Password</label>
-        @error('name')
+        @error('password')
           <div class="invalid-feedback">
             {{ message }}
           </div>
